9fe1ef6602a7325e63fbc9202c8d6f75be1f5754
3axap <uvanov977777@gmail.com>
2022-03-08 13:57:09 +0300

    go

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Slider
 
-/* Индекс слайда по умолчанию */
+
 let slideIndex = 1;
 showSlides(slideIndex, "slider_mta");
 
@@ -35,7 +35,10 @@ const prev = document.querySelector('.prev'),
 slider.forEach((item)=>{
 
   item.addEventListener('click',(e)=> {
-    console.dir(e.target);
+
+    // Обработчик события проверяет на нажимаемый слайдер и именно его перелистывает
+ 
+    // Листает назад
     if(e.target && e.target.textContent == "❮"){
       if(e.target.className == "prev mta"){
         showSlides (slideIndex += 1, "slider_mta");
@@ -47,6 +50,7 @@ slider.forEach((item)=>{
       }
      
     }
+    // Листает вперед
     if(e.target && e.target.textContent == "❯"){
       if(e.target.className == "next mta"){
         showSlides (slideIndex -= 1, "slider_mta");
@@ -62,5 +66,55 @@ slider.forEach((item)=>{
   });
 });
 
+
+// sticky hat
+const menu = document.querySelector('.menu'),
+      up = document.querySelector('.up_block');
+// обработчик события на window
+window.addEventListener('scroll', ()=>{
+  
+  // eсли scroll top больше 50 меняем поизицию nav
+  if(document.documentElement.scrollTop >= 50){
+      menu.style.position = 'sticky';
+      menu.style.top = '0px';
+  }
+    console.log(document.documentElement.scrollTop);
+    if(document.documentElement.scrollTop >= 350){
+      up.style.display = 'block';
+      up.style.position = 'fixed';
+      up.style.top = '35px';
+      up.style.right = '1px';
+  }else{
+    up.style.display = 'none';
+  }
+});
+// Функция установки скролла
+function getScrollTop(i){
+  document.documentElement.scrollTop = i;
+}
+
+// обработчик события для кнопки вверх
+up.addEventListener('click', ()=>{
+  getScrollTop(0);
+});
+
+
+// навигационное меню
+menu.addEventListener('click', (e)=>{
+ // проверяем куда нажимает пользователь и после скролим на выбранный заголовок
+   if(e.target && e.target.className == 'mta'){
+    getScrollTop(350);
+   }
+
+   if(e.target && e.target.className == 'lua'){
+    getScrollTop(950);
+   }
+   if(e.target && e.target.className == 'rp'){
+    getScrollTop(1400);
+   }
+   if(e.target && e.target.className == 'devgaming'){
+    getScrollTop(2820);
+   }
+});
 
 
